1a0b7a1f978f8591ccd7887d1ba8f3e3a12057bc
jbpeconillo <jbpeconcillo@gmail.com>
2026-05-20 11:23:57 +0800

    CI Updates adn Workflows

--- frontend/playwright.config.ts
+++ frontend/playwright.config.ts
@@ -72,9 +72,8 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'npm run dev',
-    url: 'http://localhost:3000',
-    reuseExistingServer: !process.env.CI,
-    timeout: 120000,
-  },
+     command: 'npm run start',
+     url: 'http://localhost:3000',
+     reuseExistingServer: !process.env.CI,
+   },
 });
